7de072acc8b02e6bd7c518fe29b5b55f99dae31e
julie-lambert <julie.lambert@laplateforme.io>
2024-01-30 10:23:24 +0100

    job15

---
+++ job-15/index.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\Category;
+use App\Electronic;
+use App\Clothing;
+
+
+
+//$category = new Category(1,'T-shirt',  'A beatiful T-shirt',  new DateTime(), new DateTime());
+//$product1 = new Product(1,'T-shirt', ['https://picsum.photos/200/300'], 1000, 'A beatiful T-shirt', 10, new DateTime(), new DateTime(),1 );
+
+
+$electronic = new Electronic();
+$clothing = new Clothing();
+
+// findOneById -------------------------
+// $newClothing = $clothing->findOneById(1);
+// $newElectronic = $electronic->findOneById(3);
+
+// findAll -------------------------
+// $newClothing = $clothing->findAll();
+// $newElectronic = $electronic->findAll();
+
+// create -------------------------
+// $clothing->create();
+// $electronic->create();
+
+// update -------------------------
+// $clothing->update();
+// $electronic->update();
+
+
+// var_dump($newClothing);
+// var_dump($newElectronic);
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 06</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ job-15/src/Category.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App;
+
+use App\Abstract\Product;
+use DateTime;
+use PDO;
+
+class Category{
+
+    private ?int $id;
+    private ?string $name;
+    private ?string $description;
+    private ?DateTime $createdAt;
+    private ?DateTime $updatedAt;
+    private ?PDO $db;
+
+    public function __construct($id=null, $name=null, $description=null, $createdAt=null, $updatedAt=null)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+
+ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+     
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+   
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+   
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+ 
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+
+      /**
+     * Get the value of db
+     */ 
+    public function getDb()
+    {
+        return $this->db;
+    }
+
+    /**
+     * Set the value of db
+     *
+     * @return  self
+     */ 
+    public function setDb($db)
+    {
+        $this->db = $db;
+
+        return $this;
+    }
+
+
+    public function getProducts()
+    {
+        $request= $this->db->prepare("SELECT * FROM product WHERE category_id = :id");
+        $request->bindValue(':id', $this->id, PDO::PARAM_INT);
+        $request->execute();
+        $results= $request->fetchAll(PDO::FETCH_ASSOC);
+
+       
+        $productArray=[];
+        foreach($results as $result) {
+            $productArray[]=new Product(
+                $result['id'],
+                $result['name'],
+                json_decode($result['photos']),
+                $result['price'],
+                $result['description'],
+                $result['quantity'],
+                new DateTime($result['createdAt']),
+                new DateTime($result['updatedAt']),
+                $result['category_id'],
+            );
+
+        }
+        return $productArray;
+    }
+
+   
+
+}
+
+
+
+
+
+?>
